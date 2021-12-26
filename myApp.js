51fd839e297dacc358950e97dc038f0b5bfd638b
ojppresido <you@ojppresido@gmail.com>
2021-12-26 20:28:16 +0100

    hsts1

--- myApp.js
+++ myApp.js
@@ -7,7 +7,8 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-app.use(helmet.hsts());
+var ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 
 
 
